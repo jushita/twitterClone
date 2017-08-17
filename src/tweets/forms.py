18a01c18808f347b2b16e39a472d79636db62c1a
Jushita Rahman <jushitaa@gmail.com>
2017-08-17 14:18:24 -0400

    forms validation donee

--- src/tweets/forms.py
+++ src/tweets/forms.py
@@ -12,10 +12,10 @@ class TweetModelForm(forms.ModelForm):
             "content"
         ]
         #exclude = ['user']
-
+'''
     def clean_content(self, *args, **kwargs):
         content = self.cleaned_data.get("content")
         if content == "abc":
             raise forms.ValidationError("Cannot be ABC")
         return content
-    
+'''
